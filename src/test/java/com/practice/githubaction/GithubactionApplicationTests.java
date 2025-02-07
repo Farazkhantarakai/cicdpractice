7bcfc1f0dd40f472493803f7a7a5e893f6e8bd81
Faraz khan <farazkhantaraka@gmail.com>
2025-02-07 11:52:50 +0500

    workflowchange

--- src/test/java/com/practice/githubaction/GithubactionApplicationTests.java
+++ src/test/java/com/practice/githubaction/GithubactionApplicationTests.java
@@ -6,6 +6,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class GithubactionApplicationTests {
 
+	GithubactionApplicationTests(){}
+
+
 	@Test
 	void contextLoads() {
 	}
